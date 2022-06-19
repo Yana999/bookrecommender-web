754267a872cc2b05ab33eb5b0315a3949d1b7671
AlexaEgorova <egorova.a007@gmail.com>
2022-06-19 15:28:14 +0300

    fix anyHttp

--- app/config.py
+++ app/config.py
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
         "http://localhost:8000",  # type: ignore
         "https://localhost:3000",  # type: ignore
         "https://localhost:8000",  # type: ignore
-        "https://bookrecwebapp.herokuapp.com"
+        "https://bookrecwebapp.herokuapp.com/"
     ]
 
     PROJECT_NAME: str = "Book recommender model API"
